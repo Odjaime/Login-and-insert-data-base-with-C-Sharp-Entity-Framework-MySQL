e78f3a8b266ac05807f8be634e0e55ca16453b70
Odjaime <odjaimethalmo@hotmail.com>
2017-06-15 00:03:24 -0300

    Data Base

---
+++ DataBase MySQL/login_navio.sql
@@ -0,0 +1,59 @@
+-- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
+--
+-- Host: localhost    Database: login
+-- ------------------------------------------------------
+-- Server version	5.7.18-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `navio`
+--
+
+DROP TABLE IF EXISTS `navio`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `navio` (
+  `idNavio` int(11) NOT NULL AUTO_INCREMENT,
+  `vesselName` varchar(70) DEFAULT NULL,
+  `flag` varchar(50) DEFAULT NULL,
+  `callSign` varchar(15) DEFAULT NULL,
+  `imo` int(11) DEFAULT NULL,
+  `loa` int(11) DEFAULT NULL,
+  `nrt` int(11) DEFAULT NULL,
+  `grt` int(11) DEFAULT NULL,
+  `dwt` int(11) DEFAULT NULL,
+  `builtYear` year(4) DEFAULT NULL,
+  PRIMARY KEY (`idNavio`)
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `navio`
+--
+
+LOCK TABLES `navio` WRITE;
+/*!40000 ALTER TABLE `navio` DISABLE KEYS */;
+INSERT INTO `navio` VALUES (12,'ALEXANDROS M','MALTA','9 H G W 8',9203801,183,13,29,49,1999),(16,'Rocha','Brasil','12d3',1234,1234,1234,1234,1234,0000),(17,'Marcio','Argentina','1s32d',1234,1234,1234,1234,1234,0000);
+/*!40000 ALTER TABLE `navio` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-06-13 21:18:13
